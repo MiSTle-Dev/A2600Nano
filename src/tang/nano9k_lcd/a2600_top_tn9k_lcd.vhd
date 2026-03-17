d9fbf55ba1e5e95aacecc81478c16266af90d2a7
Stefan Voss <stefan.voss@live.de>
2026-03-17 22:47:59 +0100

    hdmi improve

--- src/tang/nano9k_lcd/a2600_top_tn9k_lcd.vhd
+++ src/tang/nano9k_lcd/a2600_top_tn9k_lcd.vhd
@@ -138,7 +138,7 @@ signal spi_io_din     : std_logic;
 signal spi_io_ss      : std_logic;
 signal spi_io_clk     : std_logic;
 signal spi_io_dout    : std_logic;
-signal system_wide_screen : std_logic;
+signal system_screen  : std_logic_vector(1 downto 0);
 signal leds           : std_logic_vector(5 downto 0);
 signal system_leds    : std_logic_vector(1 downto 0);
 signal db9_joy        : std_logic_vector(5 downto 0);
@@ -456,7 +456,7 @@ mcu_osd_strobe => mcu_osd_strobe,
 mcu_data  => mcu_data_out,
 
 -- values that can be configure by the user via osd
-system_wide_screen => system_wide_screen,
+system_screen => system_screen,
 system_scanlines => system_scanlines,
 system_volume => system_volume,
 
@@ -947,7 +947,7 @@ module_inst: entity work.sysctrl
   system_reset        => system_reset,
   system_scanlines    => system_scanlines,
   system_volume       => system_volume,
-  system_wide_screen  => system_wide_screen,
+  system_screen       => system_screen,
   system_port_1       => port_1_sel,
   system_port_2       => port_2_sel,
   system_paddle       => paddle_inv,
